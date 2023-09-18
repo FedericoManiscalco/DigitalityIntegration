7b61abce378bb82880e1f91e121abe3a88502121
rustoboy <132679857+rustoboy@users.noreply.github.com>
2023-09-18 10:52:48 +0200

    Update EmployeeController.java

--- src/main/java/com/controller/EmployeeController.java
+++ src/main/java/com/controller/EmployeeController.java
@@ -26,7 +26,7 @@ public class EmployeeController {
 
 	@GetMapping("/getHelloWorld")
 	public String getHelloWorld() {
-		return "porco pisellino";
+		return "Hello world";
 	}
 	// vediamo se funge
 	// vediamo se funge2.0
